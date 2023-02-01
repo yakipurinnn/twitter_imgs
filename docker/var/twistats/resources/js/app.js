dc92bb4b5e1c1bc7c7c29da3af3ad18a0ee899b7
yakipurinnn <takanori.06157@icloud.com>
2023-02-02 03:37:07 +0900

    vue.js導入

--- docker/var/twistats/resources/js/app.js
+++ docker/var/twistats/resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+console.log(app.version);
+
+app.mount("#app");
--- docker/var/twistats/resources/views/welcome.blade.php
+++ docker/var/twistats/resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -124,6 +125,10 @@
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                    </div>
+                    
+                    <div id="app">
+                        <app></app>
                     </div>
                 </div>
             </div>
